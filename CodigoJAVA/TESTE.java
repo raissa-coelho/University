d2b1ac38aca9063b0d2c4232d147b9997d4953de
Bianca Nunes Coelho <72714890+BiancaNCoelho@users.noreply.github.com>
2021-05-24 17:47:02 -0300

    Update and rename BatalhaNaval.java to TESTE.java

---
+++ CodigoJAVA/TESTE.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class TESTE extends JFrame{
+    public static void main (String args[]){
+        
+        MainMenu menu = new MainMenu();
+
+        menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
