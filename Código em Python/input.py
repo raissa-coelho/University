1cb5b6c34eeccd020e2946e50d3a30ef0bedd688
Raissa Coelho <72715305+raissa-coelho@users.noreply.github.com>
2021-08-19 18:56:33 -0300

    Create input.py

---
+++ Código em Python/input.py
@@ -0,0 +1,11 @@
+ #aprendendo a fazer entradas
+
+escreve = input("WRITTTTTTE: ")  #mesmo se for numero, python faz string
+print(escreve)
+
+numero = int(input("Escolha 0 ou 1: ")) #typecasting para int
+
+if(numero == 0):
+    print("Você venceu!")
+
+str(numero) #transforma um inteiro em uma string
