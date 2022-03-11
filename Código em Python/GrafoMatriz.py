2d6b3ae528540c4089ee0234bec09a5a98652559
Bianca Nunes Coelho <72714890+BiancaNCoelho@users.noreply.github.com>
2022-03-11 19:32:14 -0300

    Add files via upload

---
+++ Código em Python/GrafoMatriz.py
@@ -0,0 +1,57 @@
+#Grafo com pesos/ Tipo Matriz
+
+from ipaddress import collapse_addresses
+
+
+class GrafoM():
+
+    def Def(self, col, lin):
+        matriz = [ ( [0] * col) for i in range(lin) ]
+        self.matriz = matriz
+        return matriz
+    
+    def AddPeso(self, col, lin, peso):
+        self.matriz[lin][col] = peso
+        return self.matriz
+    
+    def Imprimi(self, matriz):
+        if matriz == None:
+            print("Vazia.")
+        else:
+            print(matriz)
+
+def MENU():
+    print("--MENU--")
+    print("1.Definir Grafo(Matriz)")
+    print("2.Adiconar Peso")
+    print("3.Imprimir")
+    print("4.Sair")
+
+def main():
+    MENU()
+    Matriz = None
+    mm = GrafoM()
+    op = int(input("Op:"))
+
+    while op != 4:
+        if op == 1:
+            col = int(input("Coluna:"))
+            lin = int(input("Linha: "))
+            Matriz = mm.Def(col, lin)
+        elif op == 2:
+            col = int(input("Coluna:"))
+            lin = int(input("Linha: "))
+            peso = int(input("Peso: "))
+            Matriz = mm.AddPeso(col,lin,peso)
+        elif op == 3:
+            mm.Imprimi(Matriz)
+        else:
+            print("Invalido.")
+        
+        print()
+        MENU()
+        op = int(input("Op:"))
+
+if __name__ == "__main__":
+    main()
+    
