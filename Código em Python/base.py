ac2c907f6c3d897e15344eb5920ef877278ca316
Raissa Coelho <72715305+raissa-coelho@users.noreply.github.com>
2022-03-12 21:57:27 -0300

    Add files via upload
    Criando uma db usando SQLite no Python

---
+++ Código em Python/base.py
@@ -0,0 +1,19 @@
+import sqlite3
+from sqlite3 import Error
+
+from django.db import connection
+
+def create_conection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Conexão feita com sucesso.")
+    except Error as e:
+        print(f"O erro '{e}' ocorreu.")
+    
+    return connection
+
+if __name__ == '__main__':
+    create_conection(r"C:\Users\raiss\Downloads\Programas\Python\SQLfiles\Database\pythonsqlite.db")
+
+// criando uma Database em python
