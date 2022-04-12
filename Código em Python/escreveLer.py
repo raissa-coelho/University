fbd1b300ed1d039b797b5f754cd8d265530a9b1b
Bianca Nunes Coelho <72714890+BiancaNCoelho@users.noreply.github.com>
2022-04-12 17:13:07 -0300

    Add files via upload

--- Código em Python/escreveLer.py
+++ Código em Python/escreveLer.py
@@ -1,10 +1,15 @@
 #escrever e ler files por python
 
-fo = open('teste.txt', 'r+')
+with open('tes.txt', 'w') as writer:
+    i = 0
+    while i < 10:
+       writer.write("\n")
+       writer.write(str(i))
+       i += 1
 
-print("Nome do arquivo: " , fo.name)
-fo.write("Reginaldo--58\n")
-fo.readline()
-fo.write("Bianca--26\n")
+with open('tes.txt', 'r') as reader:
+    print("{}" .format(reader.name))
+    for linha in reader:
+        print(linha, end = '') #The end='' is to prevent Python from adding an additional newline to the text that is being printed and only print what is being read from the file.
 
-fo.close()
+
