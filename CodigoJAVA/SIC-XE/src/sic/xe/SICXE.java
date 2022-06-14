fb6dd537c31300b6bacd0551740fdd131d31260e
Bianca Nunes Coelho <72714890+BiancaNCoelho@users.noreply.github.com>
2022-06-14 16:31:29 -0300

    Add files via upload

---
+++ CodigoJAVA/SIC-XE/src/sic/xe/SICXE.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sic.xe;
+
+/**
+ *
+ * @author BIANCA
+ */
+public class SICXE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         JANELA janela = new JANELA();
+         janela.setVisible(true);
+
+    }
+    
+}
